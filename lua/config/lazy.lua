3846a18311150dbe5a07e434e21a3266de58c7b5
dat truong <thelegendaglx@gmail.com>
2025-03-17 22:13:03 +0700

    update keymap for completion

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -49,6 +49,6 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { "catppuccin/nvim" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- checker = { enabled = true },
 })
 
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -10,17 +10,50 @@ return {
 		"rafamadriz/friendly-snippets",
   },
   config = function()
-		 local luasnip = require("luasnip")
-		 -- Load default VSCode-style snippets (optional)
-		 require("luasnip.loaders.from_vscode").lazy_load()
-		 luasnip.snippets = {
-			 all = {
-				 luasnip.parser.parse_snippet("trigger", "This is a snippet with a ${1:placeholder}."),
-			 },
-		 }
+		local luasnip = require("luasnip")
+		-- Load default VSCode-style snippets (optional)
+		require("luasnip.loaders.from_vscode").lazy_load()
+		luasnip.snippets = {
+			all = {
+				luasnip.parser.parse_snippet("trigger", "This is a snippet with a ${1:placeholder}."),
+			},
+		}
 
 		 local cmp = require("cmp")
-	
+		 local function selectItem(number)
+			 cmp.mapping(function(fallback)
+				 print("dat number" ..number)
+				 if luasnip.jumpable(number) then
+					 luasnip.jump(number)
+				 elseif cmp.visible() then
+					 if number > 0 then
+						 cmp.select_next_item()
+					 else
+						 cmp.select_prev_item()
+					 end
+				 else
+					 fallback()
+				 end
+			 end, { "i", "s" })
+		 end
+		 local function selectNext(fallback)
+			 if luasnip.jumpable(1) then
+				 luasnip.jump(1)
+			 elseif cmp.visible() then
+				 cmp.select_next_item()
+			 else
+				 fallback()
+			 end
+		 end
+		 local function selectPrev(fallback)
+			 if luasnip.jumpable(-1) then
+				 luasnip.jump(-1)  -- Jump to previous placeholder
+			 elseif cmp.visible() then
+				 cmp.select_prev_item()  -- Move up the completion list
+			 else
+				 fallback()  -- If no completion, fallback to normal Shift+Tab behavior (e.g., outdent)
+			 end
+		 end
 		 cmp.setup({
 			 sources = {
 				 -- { name = "nvim_lsp" }
@@ -48,16 +81,12 @@ return {
 						 fallback()  -- If no completion, fallback to normal tab behavior (e.g., indent)
 					 end
 				 end, { "i", "s" }),
-				 ["<S-Tab>"] = cmp.mapping(function(fallback)
-					 if luasnip.jumpable(-1) then
-						 luasnip.jump(-1)  -- Jump to previous placeholder
-					 elseif cmp.visible() then
-						 cmp.select_prev_item()  -- Move up the completion list
-					 else
-						 fallback()  -- If no completion, fallback to normal Shift+Tab behavior (e.g., outdent)
-					 end
-				 end, { "i", "s" }),
-				 ["<CR>"] = cmp.mapping.confirm({ select = true }),  -- Confirm with Enter
+				 ["<S-Tab>"] = cmp.mapping(selectPrev, {"i", "s"}),
+				 ["<CR>"] = cmp.mapping.confirm({ select = true }),  -- Confirm with Enter				 
+				 ["<C-n>"] = cmp.mapping(selectNext, {"i", "s"}),
+				 ["<C-p>"] = cmp.mapping(selectPrev, {"i", "s"}),
+				 ["<Up>"] = cmp.mapping(selectPrev, {"i", "s"}),
+				 ["<Down>"] = cmp.mapping(selectNext, {"i", "s"}),
 			 },
 			 windows = {
 				 completion = cmp.config.window.bordered(),
